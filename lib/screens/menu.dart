0872a69818a2684c280ac4f003133a5b1f0c701b
firstaqila <saffana.firsta@gmail.com>
2025-11-28 15:22:46 +0700

    logic and design for register

---
+++ lib/screens/menu.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+    MyHomePage({super.key});
+
+    @override
+        Widget build(BuildContext context) {
+            return Scaffold(
+            );
+        }
+}
